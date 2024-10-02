ead352261d8990811ee847f63368543c8c5a0801
juanr8877 <juanr8877@gmail.com>
2024-10-01 20:38:09 -0500

    actualizacion del registro de usuarios

--- backend/routers/usuarios.router.js
+++ backend/routers/usuarios.router.js
@@ -4,4 +4,6 @@ const usuariosController = require('../controllers/usuarios.controller');
 
 router.get('/', usuariosController.usuarios );
 
+router.post('/registroUsario', usuariosController.registroUsuarios );
+
 module.exports = router;
