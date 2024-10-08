503347e756617d94af77d8757c08e9fdf4469925
angel <maamaya921@soy.sena.edu.co>
2024-10-07 19:06:53 -0500

    creacion de categorias listo

--- backend/routers/categorias.router.js
+++ backend/routers/categorias.router.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 const categoriasController = require('../controllers/categorias.controller');
 
-router.get('/registroCategorias', categoriasController.registroCategorias );
-router.get('/listarCategorias', categoriasController.listarCategorias );
+router.get('/registroCategorias', categoriasController.registroCategorias);
+router.get('/listarCategorias', categoriasController.listarCategorias);
+router.post('/', categoriasController.createCateg); 
+router.post('/eliminar/:id', categoriasController.deleteCateg);
+router.post('/editar/:id', categoriasController.editarCategoria);
+
 
 module.exports = router;
