7238711ed6d844bcc7382b581e8823e374b1be01
juanr8877 <jdramirez980@soy.sena.edu.co>
2024-10-17 14:19:52 -0500

    crud clientes

--- backend/models/clientes.models.js
+++ backend/models/clientes.models.js
@@ -6,27 +6,29 @@ const schemaCliente = new Schema({
         type: String,
         required: [true, 'Ingrese el nombre completo']
     },
-    documento:{
-        type:String,
-        require: [true, 'ingrese su documento'],
-        maxLength:[11, 'ingrese un documento valido']
+    documento: {
+        type: String,
+        required: [true, 'Ingrese su documento'], 
+        unique: true, 
+        maxLength: [11, 'Ingrese un documento válido']
     },
     telefono: {
         type: String,
         required: true,
         trim: true,
-        minLength:[9, 'El telefono ingresado es muy corto'],
-        maxLength:[14, 'El telefono ingresado es muy extenso']
+        minLength: [9, 'El teléfono ingresado es muy corto'],
+        maxLength: [14, 'El teléfono ingresado es muy extenso']
     },
     correo: {
         type: String,
-        required: [true, 'Ingrese el correo']
+        required: [true, 'Ingrese el correo'],
+        match: [/.+@.+\..+/, 'Ingrese un correo electrónico válido'] 
     },
     direccion: {
         type: String,
         trim: true
     }
-});
+}, { timestamps: true }); 
 
 const clienteModel = mongoose.model("cliente", schemaCliente);
 module.exports = clienteModel;
--- backend/routers/clientes.router.js
+++ backend/routers/clientes.router.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const middlewareAutenticacion = require('../utils/middleareAutenticacion');
-
 const clientesController = require('../controllers/clientes.controller');
 
-router.get('/registroClientes',middlewareAutenticacion, clientesController.registroClientes );
-router.get('/listarClientes',middlewareAutenticacion, clientesController.listarClientes );
+router.get('/', clientesController.clientes );
+router.post('/crearCliente', clientesController.crearClientes );
+router.get('/detalleCliente/:id', clientesController.detalleClientes );
+router.post('/editarCliente/:id', clientesController.editarClientes );
+router.post('/eliminarCliente/:id', clientesController.eliminarClientes );
 
 module.exports = router;
